a768ffba75e3b2d5b54a1f4a6bec9c1d6ee1f973
ChloeArd <chloe.ardoise@gmail.com>
2021-02-02 22:21:25 +0100

    Exercice 170

--- index.php
+++ index.php
@@ -5,8 +5,9 @@
  * 2. Affichez la valeur de ce cookie avec un simple echo.
  */
 // TODO Votre code ici.
-
-
+/**setcookie("username", "Chloé");
+echo $_COOKIE["username"];
+*/
 
 /**
  * 3. Commentez le code du point 1 et 2
@@ -14,3 +15,4 @@
  * 5. Créez une page bio.php et tentez d'afficher le cookie username sur cette page.
  */
 // TODO Votre code ici.
+setcookie("username", "Chloé", time() + (86400 *2) + (3600 * 2), "/", "localhost");
